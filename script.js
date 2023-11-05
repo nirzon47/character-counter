1b3a91030165659d19bcaf231deb13dabf687e18
Nirzon Karmakar <nirzon47@gmail.com>
2023-11-05 16:36:27 +0530

    added event listeners

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ const textarea = document.getElementById('textarea')
 const range = document.getElementById('range')
 const count = document.getElementById('count')
 const remaining = document.getElementById('remaining')
+
+// Event Listeners
+textarea.addEventListener('input', changeCount())
